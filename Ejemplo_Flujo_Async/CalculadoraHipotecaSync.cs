2ba9c059f21c51ecdfacb56c2ebbd614271d261f
braian <braian.granero29@gmail.com>
2023-03-23 12:54:27 -0300

    Agregar archivos de proyecto.

---
+++ Ejemplo_Flujo_Async/CalculadoraHipotecaSync.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_Flujo_Async
+{
+    public class CalculadoraHipotecaSync
+    {
+        public static int ObtenerAniosDeVidaLaboral()
+        {
+            Console.WriteLine("\nObteniendo años de vida laboral...");
+            Task.Delay(5000).Wait(); //esperamos 5 segundos
+            return new Random().Next(1, 35); //devolvemos un valor random entre 1 y 35
+        }
+
+        public static bool EsContratoIndefinido()
+        {
+            Console.WriteLine("\nVerificando si el tipo de contrato es infinido...");
+            Task.Delay(5000).Wait(); //esperamos 5 segundos
+            return (new Random().Next(0,10)) % 2 == 0;
+
+        }
+
+        public static int ObtenerSueldoNeto()
+        {
+            Console.WriteLine("\nObteniendo sueldo neto...");
+            Task.Delay(5000).Wait(); //esperamos 5 segundos
+            return new Random().Next(800, 6000);
+
+        }
+
+        public static int ObtenerGastosMensuales()
+        {
+            Console.WriteLine("\nObteniendo gastos mensuales...");
+            Task.Delay(5000).Wait(); //esperamos 5 segundos
+            return new Random().Next(200, 1000);
+
+        }
+
+        public static bool SeConedeHipoteca(
+            int aniosVidaLaboral,
+            bool tipoContrato,
+            int sueldoNeto,
+            int gastosMensuales,
+            int cantidadSolicitada,
+            int aniosPagar)
+        {
+            Console.WriteLine("\nAnalizando informacion para conceder hipoteca...");
+            if(aniosVidaLaboral < 2)
+            {
+                return false;
+            }
+
+            //obtener cuota mensual a pagar
+            var cuota = (cantidadSolicitada / aniosPagar) / 12;
+
+            if(cuota >= sueldoNeto || cuota > (sueldoNeto / 2))
+            {
+                return false;
+            }
+
+            //obtener porcentaje sobre gastos
+            var porcentajeGastosSobreSueldo = (gastosMensuales * 100) / sueldoNeto;
+
+            if(porcentajeGastosSobreSueldo > 30)
+            {
+                return false;
+            }
+
+            if(cuota + gastosMensuales >= sueldoNeto)
+            {
+                return false;
+            }
+
+            if (!tipoContrato)
+            {
+                if((cuota + gastosMensuales) > sueldoNeto / 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ Ejemplo_Flujo_Async/Program.cs
@@ -0,0 +1,90 @@
+﻿using Ejemplo_Flujo_Async;
+using System.Diagnostics;
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+//----------------------[Parte Sincrona]------------------------------------------ 
+/*Console.WriteLine("\nBienvenido a la calculadora de hipotecas sincorna");
+
+var aniosVidaLaboral = CalculadoraHipotecaSync.ObtenerAniosDeVidaLaboral();
+Console.WriteLine($"\nAños de vida laboral obtenido: {aniosVidaLaboral}");
+
+var contratoIndefinido = CalculadoraHipotecaSync.EsContratoIndefinido();
+Console.WriteLine($"\nEs contrato indefinido: {(contratoIndefinido ? "Si" : "No")}");
+
+var sueldoNeto = CalculadoraHipotecaSync.ObtenerSueldoNeto();
+Console.WriteLine($"\nSueldo neto obtenido: {sueldoNeto}€");
+
+var gastosMensuales = CalculadoraHipotecaSync.ObtenerGastosMensuales();
+Console.WriteLine($"\nGastos mensuales obtenido: {gastosMensuales}€");
+
+var hipotecaConcedida = CalculadoraHipotecaSync.SeConedeHipoteca(
+    aniosVidaLaboral, contratoIndefinido, sueldoNeto, gastosMensuales, cantidadSolicitada: 50000, aniosPagar: 30);
+
+var resultado = hipotecaConcedida ? "Aprobada" : "Rechazada";
+
+Console.WriteLine($"\nAnalisis finalizado.\nSu solicitudo de hipoteca fue: {resultado}");
+
+
+stopwatch.Stop();
+
+Console.WriteLine($"\nLa operacion a durado: {stopwatch.Elapsed}");*/
+
+//----------------------[Parte Asincrona]------------------------------------------ 
+Console.WriteLine("\nBienvenido a la calculadora de hipotecas Asincorna");
+
+var aniosVidaLaboralTask = CalculadoraHipotecaAsync.ObtenerAniosDeVidaLaboral();
+var contratoIndefinidoTask = CalculadoraHipotecaAsync.EsContratoIndefinido();
+var sueldoNetoTask = CalculadoraHipotecaAsync.ObtenerSueldoNeto();
+var gastosMensualesTask = CalculadoraHipotecaAsync.ObtenerGastosMensuales();
+
+var analisisHipotcaTask = new List<Task>
+{
+    aniosVidaLaboralTask,
+    contratoIndefinidoTask,
+    sueldoNetoTask,
+    gastosMensualesTask
+};
+
+while (analisisHipotcaTask.Any())
+{
+    Task tareaFinalizada = await Task.WhenAny(analisisHipotcaTask);
+    if (tareaFinalizada == aniosVidaLaboralTask)
+    {
+        Console.WriteLine($"\nAños de vida laboral obtenido: {aniosVidaLaboralTask.Result}");
+
+    }
+    else if (tareaFinalizada == contratoIndefinidoTask)
+    {
+        Console.WriteLine($"\nEs contrato indefinido: {(contratoIndefinidoTask.Result ? "Si" : "No")}");
+
+    }
+    else if(tareaFinalizada == sueldoNetoTask)
+    {
+        Console.WriteLine($"\nSueldo neto obtenido: {sueldoNetoTask.Result}€");
+
+    }
+    else if(tareaFinalizada == gastosMensualesTask)
+    {
+        Console.WriteLine($"\nGastos mensuales obtenido: {gastosMensualesTask.Result}€");
+
+    }
+
+    analisisHipotcaTask.Remove(tareaFinalizada); //eliminamos de la lista de tareas para ir vaciando y salir del while
+}
+
+var hipotecaConcedida = CalculadoraHipotecaAsync.SeConedeHipoteca(
+    aniosVidaLaboralTask.Result, contratoIndefinidoTask.Result, sueldoNetoTask.Result, gastosMensualesTask.Result,
+    cantidadSolicitada: 50000, aniosPagar: 30);
+
+var resultado = hipotecaConcedida ? "Aprobada" : "Rechazada";
+
+Console.WriteLine($"\nAnalisis finalizado.\nSu solicitudo de hipoteca fue: {resultado}");
+
+
+stopwatch.Stop();
+
+Console.WriteLine($"\nLa operacion a durado: {stopwatch.Elapsed}");
+
+
